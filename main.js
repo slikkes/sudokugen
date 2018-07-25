b132564e4ad3bcdc478d8b5a2871bdfd4beeb27b
slikkistvan@gmail.com <slikkistvan@gmail.com>
2018-07-25 20:31:19 +0200

    fail

---
+++ main.js
@@ -0,0 +1,124 @@
+let digits = [1,2,3,4,5,6,7,8,9];
+
+class Cell{
+	constructor(posx, posy)
+	{
+		this.x = posx;
+		this.y = posy;
+		this.col = this.x;
+		this.row = this.y;
+		this.megacell = determineMegacell(this.x, this.y);
+		this.number = 0;
+	}		
+}	
+
+let cells;
+$(()=>{
+	$("#gen").on("click",()=>{
+		console.clear();
+		cells = [];
+		resetTable();
+		
+		initCells();
+
+		setRandomCells();
+
+		cells.forEach(e=>{
+			if(e.number == 0){
+				completeCell(e);
+			}
+		})
+
+
+		viewCells();
+	})
+	
+})
+
+function initCells(){
+	for(y=0;y<9;y++){
+		for(x=0;x<9;x++){
+			cells.push(new Cell(x,y))
+		}
+	}
+}
+
+function setRandomCells()
+{
+	let q = 25;
+
+	for(let i = 0; i<q; i++){
+		let x = Math.floor(Math.random()*8);
+		let y = Math.floor(Math.random()*8);
+
+		let cell = _.find(cells,{x: x, y: y});
+
+		completeCell(cell);
+	}
+
+
+
+}
+
+
+function completeCell(cell)
+{
+
+	let availableDigits = getAvailableDigits(cell.x,cell.y);
+	let max = availableDigits.length;
+
+	let choosen = Math.floor(Math.random()*max);
+	let number = availableDigits[choosen];
+	
+	cell.number=number;
+	//cells.push(new Cell(x,y,number));
+
+}
+
+
+function determineMegacell(x,y)
+{
+	let megacell = Math.floor(x/3) + (Math.floor(y/3))*3;
+	return megacell;
+}
+
+
+function getAvailableDigits(x,y)
+{
+	let existingDigits = [];
+	let megacell = determineMegacell(x,y);
+
+	let count = 0;
+
+	cells.forEach(e=>{
+		if(e.x == x || e.y == y || e.megacell == megacell) 
+		{
+			existingDigits.push(e.number);
+		}
+	})
+
+	existingDigits=_.uniq(existingDigits);
+	return _.difference(digits,existingDigits);
+}
+
+function resetTable(){
+	for(y=0;y<9;y++){
+		for(x=0;x<9;x++){
+			$("#r"+y+" ."+x).text("");
+
+		}
+	}
+}
+
+function viewCells(){
+	cells.forEach(e=>{
+		if(e.number != 0)
+			$("#r"+e.y+" ."+e.x).text(e.number);
+	})
+}
+
+
+
+
+
+
